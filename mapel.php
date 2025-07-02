1326eb3bde390e7b4396df2dbc0fee8e47205ab2
Ferdiansyah <psocietyyy@gmail.com>
2025-07-03 02:43:19 +0700

    Update CSS

--- mapel.php
+++ mapel.php
@@ -291,6 +291,6 @@
       </div>
 
     <script src="./assets/script.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
   </body>
 </html>
