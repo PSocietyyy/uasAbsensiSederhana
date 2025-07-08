712d407629a596867fdafb625e45c485857990d2
Ferdiansyah <psocietyyy@gmail.com>
2025-07-08 14:24:17 +0700

    Push Add Nama nama kelompok | NOT ALL

--- index.php
+++ index.php
@@ -94,6 +94,17 @@
                     Muhammad Alif Dzacky S
                   </th>
                 </tr>
+                <tr
+                  class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600"
+                >
+                  <td class="px-6 py-4">12324138</td>
+                  <th
+                    scope="row"
+                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    Hasbi Fathir Ramadhan
+                  </th>
+                </tr>
               </tbody>
             </table>
           </div>
